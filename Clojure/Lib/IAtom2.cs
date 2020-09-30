ea00729dc72096b7ae1fc343355f7e24e1bb424b
Ramsey Nasser <4302217+perycyklp2@users.noreply.github.com>
2020-09-30 17:56:47 -0400

    Begin conversion of ClojureCLR into MAGIC support
    This is in preparation for MAGIC to bootstrap and self-host. The data structures
    and runtime code from ClojureCLR is extracted and repurposed as a support library
    for MAGIC. This fork removes the following, keeping certain files to minimize
    internal API differences:
    
    * The ClojureCLR compiler (replaced by MAGIC)
    * Clojure.Main (replaced by Nostrand)
    * Clojure.Source (lives in the MAGIC repository)
    * Clojure.Tests (lives in the MAGIC repository)
    * Parts of the runtime that use the DLR (replaced by Magic.Runtime)

---
+++ Clojure/Lib/IAtom2.cs
@@ -0,0 +1,35 @@
+﻿/**
+ *   Copyright (c) Rich Hickey. All rights reserved.
+ *   The use and distribution terms for this software are covered by the
+ *   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+ *   which can be found in the file epl-v10.html at the root of this distribution.
+ *   By using this software in any fashion, you are agreeing to be bound by
+ * 	 the terms of this license.
+ *   You must not remove this notice, or any other, from this software.
+ **/
+
+/**
+ *   Author: David Miller
+ **/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace clojure.lang
+{
+    public interface IAtom2 : IAtom
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "swap")]
+        IPersistentVector swapVals(IFn f);
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "swap")]
+        IPersistentVector swapVals(IFn f, Object arg);
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "swap")]
+        IPersistentVector swapVals(IFn f, Object arg1, Object arg2);
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "swap")]
+        IPersistentVector swapVals(IFn f, Object x, Object y, ISeq args);
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "reset")]
+        IPersistentVector resetVals(Object newv);
+    }
+}
